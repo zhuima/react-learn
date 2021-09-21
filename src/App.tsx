327092ca459e8db6035bf104b58089b719ab5b3b
zhuima <zhuima314@gmail.com>
2021-09-21 12:05:22 +0800

    feat: 1、add login function 2、add json-server middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
+import { LoginScreen } from "./screens/login";
 import { ProjectListScreen } from "./screens/project-list";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
